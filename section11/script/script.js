fa3af53c01ede5a49dfd25f3115e453541ea1a47
vuHieuU <117288339+vuHieuU@users.noreply.github.com>
2023-02-04 23:05:25 +0700

    Bankis APP

--- section11/script/script.js
+++ section11/script/script.js
@@ -173,6 +173,20 @@ btnTransfer.addEventListener('click', function(e){
   }
 })
 
+// vay tiền
+btnLoan.addEventListener('click', function(e){
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if(amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)){
+    currentAccount.movements.push(amount)
+
+    updateUI(currentAccount)
+    inputLoanAmount.value = '';
+  }
+})
+
 // xóa tài khoản
 
 btnClose.addEventListener('click', function(e){
