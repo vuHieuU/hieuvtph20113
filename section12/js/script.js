200f79d4251526cfb971d215c791f198a8ac5be8
vuHieuU <117288339+vuHieuU@users.noreply.github.com>
2023-02-07 00:31:21 +0700

    bankis

--- section12/js/script.js
+++ section12/js/script.js
@@ -40,35 +40,79 @@ document.addEventListener('keydown', function (e) {
 ////////////////////////////////////////////////
 
 
- // Chọn, tạo và xóa các phần tử
-        // console.log(document.documentElement);
-        // console.log(document.head);
-        // console.log(document.body);
-        // const header = document.querySelector('.header');
-        // const allSections = document.querySelectorAll('.section');
-        // console.log(allSections);
-        // document.getElementById('section--1');
-        // const allButtons = document.getElementsByTagName('button');
-        // console.log(allButtons);
-        // console.log(document.getElementsByClassName('btn'));
-        // // Creating and inserting elements
-        //  const message = document.createElement('div');
-        // message.classList.add('cookie-message');
-        // // message.textContent = 'We use cookied for improved functionality and analytics.';
-        // message.innerHTML =
-        //     'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
-            // thê html/cssm vào đầu
+// Chọn, tạo và xóa các phần tử
+        console.log(document.documentElement);
+        console.log(document.head);
+        console.log(document.body);
+        const header = document.querySelector('.header');
+        const allSections = document.querySelectorAll('.section');
+        console.log(allSections);
+        document.getElementById('section--1');
+        const allButtons = document.getElementsByTagName('button');
+        console.log(allButtons);
+        console.log(document.getElementsByClassName('btn'));
+        // Creating and inserting elements
+         const message = document.createElement('div');
+        message.classList.add('cookie-message');
+        // message.textContent = 'We use cookied for improved functionality and analytics.';
+        message.innerHTML =
+            'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+            // thêm html/cssm vào đầu
         // header.prepend(message);
-           // thêm html/css vào cuối  
-        // header.append(message);
+        //    thêm html/css vào cuối  
+        header.append(message);
         // thêm html/css vào đầu và cuối
         // header.append(message.cloneNode(true));
-          // thê html/cssm vào đầu
+        //   thê html/cssm vào đầu
         // header.before(message);
-         // thêm html/css vào cuối  
+        //  thêm html/css vào cuối  
         // header.after(message);
 
     // delete
-    // document.querySelector('.btn--close-cookie').addEventListener('click', function(){
-    //     message.remove();
-    // })
+    document.querySelector('.btn--close-cookie').addEventListener('click', function(){
+        message.remove();
+    })
+
+    // style
+
+    message.style.background = 'orange'
+    message.style.width = '120%'
+    console.log(message.style.background);
+
+
+    console.log(getComputedStyle(message).height);
+    console.log(getComputedStyle(message).color);
+
+    // thêm kích thước
+    message.style.height =parseFloat(getComputedStyle(message).height,10) + 30 +'px';
+
+    //  đổi màu với setProperty
+    document.documentElement.style.setProperty('--color-primary','blue')
+
+   // Attributes
+    const logo = document.querySelector('.nav__logo');
+    console.log(logo.alt);
+    console.log(logo.className);
+    logo.alt = 'Beautiful minimalist logo';
+    // Non-standard
+    console.log(logo.designer);
+    // lấy thuộc tính tự thêm
+    console.log(logo.getAttribute('designer'));
+    // thêm thuộc tính
+    logo.setAttribute('company', 'Bankist');
+    // lấy link
+    console.log(logo.src);
+    // lấy tên logo
+    console.log(logo.getAttribute('src'));
+    
+    const link = document.querySelector('.nav__link--btn');
+    console.log(link.href);
+    console.log(link.getAttribute('href'));
+
+    // Classes
+    logo.classList.add('c', 'j');
+    logo.classList.remove('c', 'j');
+    logo.classList.toggle('c');
+    logo.classList.contains('c'); // not includes
+    // Don't use
+    logo.clasName = 'jonas';
