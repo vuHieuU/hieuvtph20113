583778490363634562030894da131d9d4951062f
vuHieuU <117288339+vuHieuU@users.noreply.github.com>
2023-02-08 17:33:20 +0700

    193 bankis

--- section12/js/script.js
+++ section12/js/script.js
@@ -70,8 +70,6 @@ document.querySelector('.nav__links').addEventListener('click', function(e){
     }
 });
 
-
-
 ///////////////////////////////////
 ////////////////////////////////////////////////
 
@@ -184,3 +182,30 @@ document.querySelector('.nav__links').addEventListener('click', function(e){
 //     e.preventDefault();
 //      this.style.backgroundColor = rgbColor();
 // })
+
+ const h1 = document.querySelector('h1');
+
+ console.log(h1.querySelectorAll('.highlight'));
+ console.log(h1.childNodes);
+ console.log(h1.children);
+ h1.firstElementChild.style.color = 'blue';
+ h1.lastElementChild.style.color = 'orangered';
+
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('header').style.backgroundColor = "yellow"
+h1.closest('h1').style.backgroundColor = "violet"
+
+// thẻ trước sau h1
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+//kiểu thẻ trước sau h1
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+
+[...h1.parentElement.children].forEach(function(el) {
+     if(el !== h1) el.style.transform = 'scale(0.5)';
+})
